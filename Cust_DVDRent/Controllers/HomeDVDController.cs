4e7d68d498999eb38958f5057d64ffb0610bd9c4
rardianto8080 <rardianto8080@gmail.com>
2022-07-08 11:32:47 +0700

    initial

--- Cust_DVDRent/Controllers/HomeDVDController.cs
+++ Cust_DVDRent/Controllers/HomeDVDController.cs
@@ -3,15 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Cust_DVDRent.Models;
+using System.Data;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace Cust_DVDRent.Controllers
 {
     public class HomeDVDController : Controller
     {
         // GET: HomeDVD
-        public ActionResult Index()
+        public ActionResult Index(HomeDVDModels.HomeIndex<HomeDVDModels.HomeIndex> model)
         {
-            return View();
+            DataTable tableModel = new DataTable();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:26403/api/CustDVDRent/GetMovie");
+                //HTTP GET
+                var responseTask = client.GetAsync("GetMovie");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsStringAsync().Result;
+                    tableModel = (DataTable)JsonConvert.DeserializeObject(readTask, (typeof(DataTable)));
+                }
+            }
+
+            model.listMovieAvailable = ListMovie(tableModel);
+
+            model.listMovieAvailable = model.listMovieAvailable;
+
+            return View(model);
+        }
+
+        public static List<HomeDVDModels.HomeIndex> ListMovie(DataTable dt)
+        {
+            List<HomeDVDModels.HomeIndex> model = new List<HomeDVDModels.HomeIndex>();
+            DataTable tableModel = dt;
+
+            foreach (DataRow dr in tableModel.Rows)
+            {
+                model.Add(new HomeDVDModels.HomeIndex
+                {
+                    ID = dr["ID"].ToString(),
+                    Title = dr["Title"].ToString(),
+                    AgeRating = dr["Rating"].ToString(),
+                    Duration = dr["Duration"].ToString(),
+                    PictureUrl = dr["PictureURL"].ToString(),
+                    ReleaseYear = dr["ReleaseYear"].ToString(),
+                    Genre = dr["Genre"].ToString()
+                });
+            }
+
+
+            return model;
         }
     }
 }
---
+++ Cust_DVDRent/Models/HomeDVDModels.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cust_DVDRent.Models
+{
+    public class HomeDVDModels
+    {
+        public class HomeIndex
+        {
+            public string ID { get; set; }
+            public string AgeRating { get; set; }
+            public string[] GenreList { get; set; }
+            public string Genre { get; set; }
+            public string Title { get; set; }
+            public string PictureUrl { get; set; }
+            public string Duration { get; set; }
+            public string ReleaseYear { get; set; }
+        }
+
+        public class HomeIndex<T>
+        {
+            public IEnumerable<T> listMovieAvailable { get; set; }
+            //public List<T> listGenre { get; set; }
+            //public string PictureURL { get; set; }
+            //public string TrailerURL { get; set; }
+        }
+    }
+}
